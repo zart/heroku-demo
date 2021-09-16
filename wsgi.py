41beb2298a3a6cc83ebc6891f2bcb4518cd99c3b
Konstantin Zemlyak <zart@zartsoft.ru>
2021-09-16 16:07:43 +0500

    WIP

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,6 @@
+import sys
 from pprint import pformat
 
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain; charset=utf-8')])
-    return [pformat(environ).encode('utf-8')]
+    return [pformat([environ, vars(sys)]).encode('utf-8')]
